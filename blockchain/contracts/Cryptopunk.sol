97bcd7ca18b15640b19dcaf04265a8774d77ccde
SachinCoder1 <sachinhlo232@gmail.com>
2022-08-21 13:52:40 +0530

    mint function

--- blockchain/contracts/Cryptopunk.sol
+++ blockchain/contracts/Cryptopunk.sol
@@ -2,18 +2,46 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Cryptopunk is ERC721 {
+error Cryptopunk__NameAlreadyExists();
+
+contract Cryptopunk is ERC721, ERC721Enumerable {
     string[] private s_allPunks;
-    constructor() ERC721("CryptoPunk", "CPS") {
-        
-    }
+    mapping(string => bool) punksExists;
 
-    function getAllPunks() external view returns(string[] memory) {
+    constructor() ERC721("CryptoPunk", "CPS") {}
+
+    function getAllPunks() external view returns (string[] memory) {
         return s_allPunks;
     }
 
     function mint(string memory _name) external {
-       s_allPunks.push(_name);
+        if (punksExists[_name]) {
+            revert Cryptopunk__NameAlreadyExists();
+        }
+        s_allPunks.push(_name);
+        uint256 punksLength = s_allPunks.length - 1;
+        _mint(msg.sender, punksLength);
+        punksExists[_name] = true;
+    }
+
+    /* Inheriting functions */
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
